a74c1a5a90f2f12ff0b33fde3a3962f9bb1aa00d
Gary Mawdsley <gm@datasignals.co.uk>
2024-07-04 15:04:22 +0100

    initial doc site test within git repo

---
+++ docs/script.js
@@ -0,0 +1,26 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    document.querySelector('.slideshow-container').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Initialize the first slide
+showSlide(currentSlide);
